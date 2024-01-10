2348befdd158f9c16c1cf1ed78c68edd47dd68af
imhalid <imhalid@icloud.com>
2024-01-10 12:35:36 +0300

    Add initial project files and configurations

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+  <div class="view">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  gap: 1rem;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 2rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Home View
+    </div>
+</template>
+<script setup>
+
+</script>
